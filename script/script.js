0283592893811c384c53180e1bee1eb44649b756
Arsen Melqonyan <arsenn.melqonyan@gmail.com>
2022-06-28 11:26:17 +0400

    map try

--- script/script.js
+++ script/script.js
@@ -1,19 +1,45 @@
 const BOARD_SIZE = 8;
+const MATRIX = [];
 
-var table = document.createElement("table");
-for (var i = 1; i < 9; i++) {
-  var tr = document.createElement("tr");
-  for (var j = 1; j < 9; j++) {
-    var td = document.createElement("td");
-    if (i % 2 == j % 2) {
-      td.className = "white";
-    } else {
-      td.className = "black";
-    }
-    tr.appendChild(td);
-  }
-  table.appendChild(tr);
-}
-document.body.appendChild(table);
+// ha start sxmeluc 2 functiona ashxatelu demi hamar, heto avelanaluya eli, kzganq yntacqum
 
-const board = document.createElement('table');
+// hmi 2 function
+
+// 1. CreateEmptyBoard, vory et matrixi mej 8 hat arraya sarqelu, amen meki meje 8 hat zro lcni
+
+// 2. SetBunnyPosition, vory patahakan kerpov et 64 zroneric meky pti sarqi 1
+
+// const PARENT_BOARD = document.createElement("table");
+
+// var table = document.createElement("table");
+// for (var i = 1; i < 9; i++) {
+//   var tr = document.createElement("tr");
+//   for (var j = 1; j < 9; j++) {
+//     var td = document.createElement("td");
+//     if (i % 2 == j % 2) {
+//       td.className = "white";
+//     } else {
+//       td.className = "black";
+//     }
+//     tr.appendChild(td);
+//   }
+//   table.appendChild(tr);
+// }
+// document.body.appendChild(table);
+
+console.log();
+
+MATRIX.map((item) => {
+  item.Array(BOARD_SIZE)
+    .fill()
+    .map(() => Array(BOARD_SIZE).fill());
+  return MATRIX.push(item);
+});
+
+console.log(MATRIX);
+
+
+MATRIX.push(Array(9).fill().map(()=>Array(9).fill()))
+console.log(MATRIX);
+
+console.log(Array(9).fill().map(()=>Array(9).fill()));
