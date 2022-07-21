a7ea6f675466767cdba03b40cc4da6d9aa8bdbfe
Arsen Melqonyan <arsenn.melqonyan@gmail.com>
2022-07-21 21:17:51 +0400

    create board elements

--- script/script.js
+++ script/script.js
@@ -1,75 +1,58 @@
 const BOARD_SIZE = 8;
-const MATRIX = [];
-const emptySlots = 0;
+let MATRIX;
+const emptySlot = 0;
 const bunnySlot = 1;
-// ha start sxmeluc 2 functiona ashxatelu demi hamar, heto avelanaluya eli, kzganq yntacqum
-
-// hmi 2 function
-
-// 1. CreateEmptyBoard, vory et matrixi mej 8 hat arraya sarqelu, amen meki meje 8 hat zro lcni
-
-// 2. SetBunnyPosition, vory patahakan kerpov et 64 zroneric meky pti sarqi 1
-
-// const PARENT_BOARD = document.createElement("table");
-
-// var table = document.createElement("table");
-// for (var i = 1; i < 9; i++) {
-//   var tr = document.createElement("tr");
-//   for (var j = 1; j < 9; j++) {
-//     var td = document.createElement("td");
-//     if (i % 2 == j % 2) {
-//       td.className = "white";
-//     } else {
-//       td.className = "black";
-//     }
-//     tr.appendChild(td);
-//   }
-//   table.appendChild(tr);
-// }
-// document.body.appendChild(table);
+const ROOT_DIV = document.getElementById("root");
 
 const createEmptyBoardMatrix = () => {
-  MATRIX.push(
-    Array(BOARD_SIZE)
-      .fill()
-      .map(() => Array(BOARD_SIZE).fill(emptySlots, 0, BOARD_SIZE))
-  );
-
-  // return console.log(MATRIX);
+  MATRIX = new Array(BOARD_SIZE)
+    .fill(emptySlot)
+    .map(() => new Array(BOARD_SIZE).fill(emptySlot));
 };
 
 const getRandomPosition = () => {
   const row = Math.floor(Math.random() * BOARD_SIZE);
   const column = Math.floor(Math.random() * BOARD_SIZE);
 
-  return row, column;
+  return { row, column };
 };
 
 const setBunnyPosition = () => {
- const {row,column} = getRandomPosition()
+  const { row, column } = getRandomPosition();
+  console.log(MATRIX);
 
- MATRIX[row][column] = bunnySlot
+  MATRIX[row][column] = bunnySlot;
+};
+
+const createHTMLElement = (elementType, parent, className, id) => {
+  const element = document.createElement(elementType);
+  if (className) {
+    element.classList.add(className);
+  }
+  if (id) {
+    element.setAttribute("id", id);
+  }
+  parent.appendChild(element);
+};
+
+const createBoardSquare = (rowID, columnID) => {
+  const boardWrapper = document.getElementById("board-wrapper");
+  createHTMLElement("div", boardWrapper, 'square', `${rowID}${columnID}`);
+};
+
+const createBoardUi = () => {
+  createHTMLElement("div", ROOT_DIV, null, "board-wrapper");
+  MATRIX.forEach((row, rowID) => {
+    row.forEach((column, columnID) => {
+   
+      createBoardSquare(rowID, columnID);
+    });
+  });
 };
 
 const startGame = () => {
   createEmptyBoardMatrix();
-  getRandomPosition();
+  setBunnyPosition();
+  createBoardUi();
 };
 startGame();
-
-// const random = () =>{
-// console.log(MATRIX.Math.floor(Math.random() * MATRIX.length));
-// }
-
-// console.log(MATRIX);
-
-// setBunnyPosition()
-
-// var a = ['a', 'b', 'c', 'd', 'e', 'f'];
-// var randomValue = a[Math.floor(a.length * Math.random())];
-
-// console.log(randomValue);
-
-// console.log(Math.floor(Math.random() * 8 ));
-
-// random()
